2370f56a34b05fb83d6ee29b0eea7ad859e305b8
Saravanan Subramanian <SaravananSubramanian@users.noreply.github.com>
2018-10-07 20:40:40 -0600

    Updated Blog Article Example

--- HL7 Programming using Java - Tutorial Series/HL7 Programming using HAPI - Handling Binary Data/HapiExtractBinaryData.java
+++ HL7 Programming using Java - Tutorial Series/HL7 Programming using HAPI - Handling Binary Data/HapiExtractBinaryData.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.model.v24.datatype.ED;
@@ -20,8 +21,7 @@ public class HapiExtractBinaryData {
 	// this input file has been included in the source code for convenience
 	private static String OruR01MessageWithBase64EncodedPdfReportIncluded = "C:\\HL7TestInputFiles\\SaravananOruR01Message.hl7";
 
-	// paths for where the extracted PDF report will be written. Change these as
-	// needed
+	// paths for where the extracted PDF report will be written. Change these as needed
 	private static String _extractedPdfOutputDirectory = "C:\\HL7TestOutputs";
 	private static String _extractedPdfOutputPath = "C:\\HL7TestOutputs\\ExtractedPdfReport.pdf";
 
@@ -31,74 +31,77 @@ public class HapiExtractBinaryData {
 		try {
 			// instantiate a PipeParser, which handles the "traditional or default encoding"
 			PipeParser ourPipeParser = context.getPipeParser();
-			// read the message data from file and parse the string format message into a
-			// Java message object
+			// read the message data from file and parse the string format message into a Java message object
 			Message hl7Message = ourPipeParser
-					.parse(readHL7MessageFromFileAsString(OruR01MessageWithBase64EncodedPdfReportIncluded));
+					.parse(readFileDataAsString(OruR01MessageWithBase64EncodedPdfReportIncluded));
 
-			// cast to message to an ORU R01 message in order to get access to the message
-			// data
+			// cast to message to an ORU R01 message in order to get access to the message data
 			ORU_R01 oruR01Message = (ORU_R01) hl7Message;
+			
 			if (oruR01Message != null) {
 				// Display the updated HL7 message using Pipe delimited format
 				System.out.println("Parsed HL7 Message:");
 				System.out.println(ourPipeParser.encode(hl7Message));
-
-				// start retrieving the OBX segment data to get at the PDF report content
-				System.out.println("Extracting message data from parsed message..");
-				ORU_R01_ORDER_OBSERVATION ourOrderObservation = oruR01Message.getPATIENT_RESULT()
-						.getORDER_OBSERVATION();
-				ORU_R01_OBSERVATION observation = ourOrderObservation.getOBSERVATION(0);
-				OBX obxSegment = observation.getOBX();
-
-				ED encapsulatedPdfDataInBase64Format = (ED) obxSegment.getObservationValue(0).getData();
-
-				// if no encapsulated data was found, you can cease operation
-				if (encapsulatedPdfDataInBase64Format == null)
-					return;
-
-				OurBase64Helper helper = new OurBase64Helper();
-
-				System.out.println("Extracting PDF data stored in Base-64 encoded form from OBX-5..");
-				String base64EncodedByteData = encapsulatedPdfDataInBase64Format.getData().getValue();
-				byte[] extractedPdfByteData = helper.ConvertFromBase64String(base64EncodedByteData);
-
-				File directory = new File(_extractedPdfOutputDirectory);
-				if (!directory.exists()) {
-					System.out.println(
-							String.format("Creating output directory at '%s'..", _extractedPdfOutputDirectory));
-					new File(_extractedPdfOutputDirectory).mkdirs();
-				}
-
-				System.out.println(String.format(
-						"Writing the extracted PDF data to '%s'. You should be able to see the decoded PDF content..",
-						_extractedPdfOutputPath));
-
-				writeByteDataToFile(_extractedPdfOutputPath, extractedPdfByteData);
-
-				System.out.println("Extraction operation was successfully completed..");
+				
+				ExtractPdfDataAndWriteToFile(ourPipeParser, oruR01Message);
+				return;
 			}
+			
+			throw new IllegalStateException("Unable to access message data. Please check if your input file contains a valid ORU R01 message");
+			
 		} catch (Exception e) {
 			System.out.println(String.format("Error occured during Order Response PDF extraction operation -> %s",
 					e.getMessage()));
 		}
 	}
 
-	public static String readHL7MessageFromFileAsString(String fileName) throws Exception {
-		return new String(Files.readAllBytes(Paths.get(fileName)));
+	private static void ExtractPdfDataAndWriteToFile(PipeParser ourPipeParser, ORU_R01 oruR01Message) throws HL7Exception, IOException {
+		
+		ED encapsulatedPdfDataInBase64Format = getEncapsulatedDataFromObservationSegment(oruR01Message);
+
+		byte[] extractedPdfByteData = getBase64DecodedPdfByteData(encapsulatedPdfDataInBase64Format);
+
+		File directory = new File(_extractedPdfOutputDirectory);
+		if (!directory.exists()) {
+			System.out.println(String.format("Creating output directory at '%s'..", _extractedPdfOutputDirectory));
+			new File(_extractedPdfOutputDirectory).mkdirs();
+		}
+
+		System.out.println(String.format(
+				"Writing the extracted PDF data to '%s'. You should be able to see the decoded PDF content..",
+				_extractedPdfOutputPath));
+
+		writeByteDataToFile(_extractedPdfOutputPath, extractedPdfByteData);
+
+		System.out.println("Extraction operation was successfully completed..");
+	}
+
+	private static byte[] getBase64DecodedPdfByteData(ED encapsulatedPdfDataInBase64Format) {
+		OurBase64Helper helper = new OurBase64Helper();
+		System.out.println("Extracting PDF data stored in Base-64 encoded form from OBX-5..");
+		String base64EncodedData = encapsulatedPdfDataInBase64Format.getData().getValue();
+		byte[] extractedPdfByteData = helper.ConvertFromBase64String(base64EncodedData);
+		return extractedPdfByteData;
+	}
+
+	private static ED getEncapsulatedDataFromObservationSegment(ORU_R01 oruR01Message) {
+		// start retrieving the OBX segment data to get at the PDF report content
+		System.out.println("Extracting message data from parsed message..");
+		ORU_R01_ORDER_OBSERVATION ourOrderObservation = oruR01Message.getPATIENT_RESULT().getORDER_OBSERVATION();
+		ORU_R01_OBSERVATION observation = ourOrderObservation.getOBSERVATION(0);
+		OBX obxSegment = observation.getOBX();
+		ED encapsulatedPdfDataInBase64Format = (ED) obxSegment.getObservationValue(0).getData();
+		return encapsulatedPdfDataInBase64Format;
 	}
 
 	// Note: There are better/easier methods for writing to file in the newer
-	// versions of Java
-	// I am using this simply to be backwards compatible with JDK 1.4
+	// versions of Java. I am using this simply to be backwards compatible with JDK 1.4
 	private static void writeByteDataToFile(String destinationFile, byte[] byteData) throws IOException {
-
 		FileOutputStream fileOuputStream = null;
 
 		try {
 			fileOuputStream = new FileOutputStream(destinationFile);
 			fileOuputStream.write(byteData);
-
 		} catch (IOException e) {
 			// simply re-throw the exception for now
 			throw e;
@@ -107,7 +110,10 @@ public class HapiExtractBinaryData {
 				fileOuputStream.close();
 			}
 		}
-
+	}
+	
+	public static String readFileDataAsString(String fileName) throws Exception {
+		return new String(Files.readAllBytes(Paths.get(fileName)));
 	}
 
 }
